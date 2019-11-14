aebf0b3779789102f33c66d99a8539bef472fc87
wx <xxssemail@163.com>
2019-11-14 15:50:00 +0800

    添加用户

---
+++ public/assets/js/user.js
@@ -0,0 +1,15 @@
+$('#userForm').on('submit', function() {
+    let formData = $(this).serialize();
+    $.ajax({
+        type: 'post',
+        url: '/users',
+        data: formData,
+        success: function() {
+            location.reload();
+        },
+        error: function() {
+            alert('用户添加失败');
+        }
+    })
+    return false;
+})
