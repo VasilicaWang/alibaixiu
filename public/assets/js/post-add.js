eafe0dfae4558d1ce1965a17e58bb67fa00fb012
wx <xxssemail@163.com>
2019-11-16 16:07:14 +0800

    文章图片上传

--- public/assets/js/post-add.js
+++ public/assets/js/post-add.js
@@ -7,3 +7,20 @@ $.ajax({
         $('#category').html(html);
     }
 })
+
+// 上传封面图
+$('#feature').on('change', function() {
+    let file = this.files[0];
+    let formData = new FormData();
+    formData.append('cover', file);
+    $.ajax({
+        type: 'post',
+        url: '/upload',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function(response) {
+            $('#thumbnail').val(response[0].cover)
+        }
+    })
+})
