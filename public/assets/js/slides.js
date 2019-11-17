4648e299b1f4233b5e8de631f0655ee77500953f
wx <xxssemail@163.com>
2019-11-17 15:51:50 +0800

    轮播图片上传、数据添加、删除

---
+++ public/assets/js/slides.js
@@ -0,0 +1,54 @@
+// 图片上传功能
+$('#file').on('change', function() {
+    let formData = new FormData();
+    formData.append('image', this.files[0]);
+    $.ajax({
+        type: 'post',
+        url: '/upload',
+        data: formData,
+        processData: false,
+        contentType: false,
+        success: function(response) {
+            console.log(response[0].image);
+            $('#image').val(response[0].image);
+        }
+    })
+})
+
+// 获取轮播图列表
+$.ajax({
+    type: 'get',
+    url: '/slides',
+    success: function(response) {
+        console.log(response);
+        let html = template('slidesTpl', {data: response});
+        $('#slidesBox').html(html);
+    }
+})
+
+// 添加轮播图
+$('#slidesForm').on('submit', function() {
+    let formData = $(this).serialize();
+    console.log(formData);
+    $.ajax({
+        type: 'post',
+        url: '/slides',
+        data: formData,
+        success: function(response) {
+            location.reload();
+        }
+    })
+    return false;
+})
+
+// 删除操作
+$('#slidesBox').on('click', '.delete', function() {
+    let id = $(this).attr('data-id');
+    $.ajax({
+        type: 'delete',
+        url: `/slides/${id}`,
+        success: function() {
+            location.reload();
+        }
+    })
+})
