2a376ccf9e8d8d915f02ce5551ba990d314ceb15
wx <xxssemail@163.com>
2019-11-17 15:07:31 +0800

    评论功能完善

--- public/assets/js/comments.js
+++ public/assets/js/comments.js
@@ -3,7 +3,6 @@ $.ajax({
     type: 'get',
     url: '/comments',
     success: function (response) {
-        console.log(response);
         let commentsHtml = template('commentsTpl', response);
         $('#commentsBox').html(commentsHtml);
         let pageHtml = template('pageTpl', response);
@@ -27,3 +26,33 @@ function changePage(page) {
         }
     })
 }
+
+// 改变评论状态
+$('#commentsBox').on('click', '.status', function () {
+    let state = $(this).attr('data-status');
+    let id = $(this).attr('data-id');
+    $.ajax({
+        type: 'put',
+        url: `/comments/${id}`,
+        data: {
+            state: state == 0 ? 1 : 0
+        },
+        success: function () {
+            location.reload();
+        }
+    })
+})
+
+// 删除评论
+$('#commentsBox').on('click', '.delete', function () {
+    if (confirm('确认要删除吗？')) {
+        let id = $(this).attr('data-id');
+        $.ajax({
+            type: 'delete',
+            url: `/comments/${id}`,
+            success: function () {
+                location.reload();
+            }
+        })
+    }
+})
