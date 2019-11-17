da34b70cf59ad456a35a5402c47824fceb3c70dd
wx <xxssemail@163.com>
2019-11-17 18:51:37 +0800

    登录用户信息展示

--- public/assets/js/common.js
+++ public/assets/js/common.js
@@ -19,3 +19,15 @@ function formateDate(date) {
     date = new Date(date);
     return date.getFullYear() + '-' + (date.getMonth() + 1) + '-' + date.getDate();
 }
+
+// 更改用户头像
+let userId = JSON.parse(localStorage.getItem('user'))._id;
+$.ajax({
+    type: 'get',
+    url: `/users/${userId}`,
+    success: function(response) {
+        // console.log(response);
+        $('.profile .avatar').attr('src', response.avatar);
+        $('.profile .name').html(response.nickName);
+    }
+})
