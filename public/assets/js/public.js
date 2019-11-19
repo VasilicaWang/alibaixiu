79a8e000a0466fa4dc141f5479fc5b487fc7ce4a
wx <xxssemail@163.com>
2019-11-19 11:23:00 +0800

    最新评论

--- public/assets/js/public.js
+++ public/assets/js/public.js
@@ -1,8 +1,9 @@
+// 随机推荐
 $.ajax({
-    type: 'get',
-    url: '/posts/random',
-    success: function (response) {
-        let randomTpl = `
+  type: 'get',
+  url: '/posts/random',
+  success: function (response) {
+    let randomTpl = `
             {{each data}}
             <li>
             <a href="detail.html?id={{$value._id}}">
@@ -15,7 +16,39 @@ $.ajax({
             </li>
             {{/each}}
         `;
-        let html = template.render(randomTpl, {data: response});
-        $('#randomBox').html(html);
-    }
+    let html = template.render(randomTpl, {
+      data: response
+    });
+    $('#randomBox').html(html);
+  }
+})
+
+// 最新评论
+$.ajax({
+  type: 'get',
+  url: '/comments/lasted',
+  success: function (response) {
+    console.log(response);
+    let lastedCommentsTpl = `
+    {{each data}}
+    <li>
+    <a href="javascript:;">
+      <div class="avatar">
+        <img src="{{$value.author.avatar}}" alt="">
+      </div>
+      <div class="txt">
+        <p>
+          <span>{{$value.author.nickName}}</span>{{$imports.formateDate($value.createAt)}}说:
+        </p>
+        <p>{{$value.content}}</p>
+      </div>
+    </a>
+    </li>
+    {{/each}}
+    `;
+    let html = template.render(lastedCommentsTpl, {
+      data: response
+    });
+    $('#lastedCommentsBox').html(html);
+  }
 })
